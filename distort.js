9f591814134840fdb2ed32c42f79217ca32db11e
Maarten Dings <maarten.dings@cxcompany.com>
2020-01-03 09:15:49 +0100

    adding logo

---
+++ distort.js
@@ -0,0 +1,41 @@
+let orig, sib
+let runs = 0
+const charSet = '0+>AE-$#@%&*'
+
+const distort = target => {
+    // const curr = e.target
+    orig = target.innerText
+    sib = setInterval(() => text(target), 500)
+}
+
+const undistort = e => {
+    clearInterval(sib)
+    e.target.innerText = orig
+}
+
+const text = current => {
+    if (runs >= 2) {
+        runs = 0
+        current.innerText = orig
+        return
+    }
+
+    const chars = current.innerText.split('')
+    const rand = Math.floor(Math.random() * (chars.length))
+    const randRep = Math.floor(Math.random() * (charSet.length))
+    if(chars[rand] != charSet[randRep] && chars[rand] != ' ') {
+        chars[rand] = charSet[randRep]
+    } else {
+        text(current)
+    }
+
+    current.innerText = chars.join('')
+    runs += 1
+}
+
+const distortElms = document.querySelectorAll('.distort')
+distortElms.forEach(elm => {
+    distort(elm)
+    // elm.addEventListener('mouseenter', distort)
+    // elm.addEventListener('mouseleave', undistort)
+})
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import Swiper from 'swiper'
+import './distort'
+
 const [red, green, blue] = [146, 154, 233]
 const section1 = document.body
 window.addEventListener('scroll', () => {
@@ -8,50 +10,6 @@ window.addEventListener('scroll', () => {
 //   section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
 })
 //ONLOAD
-$(function(){
-    //DECLARE GLOBAL VARIABLE FOR USE IN HANDLERS
-    var orig, sib;
-    var runs = 0;
-    var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+>?-$#@%&*';
-    
-    //RUN JS WHEN 'DISTORT' IS HOVERED
-    $('.distort').hover(function(){
-        var curr = $(this);
-        orig = $(this).text();
-        sib = setInterval(function(){
-          distortText(curr);
-        }, 100);
-    }, function(){
-      //RESET THE ORIGINAL TEXT
-      clearInterval(sib);
-      $(this).text(orig);
-    });
-   
-    function distortText(i){
-      //MAINTAINS SOME READABILITY IN THE TEXT BY ONLY ALLOWING 3 CHARACTERS TO BE DISTORTED
-      if (runs >= 2){
-        runs = 0;
-        i.text(orig);
-        return;
-      }
-      //GET EACH INDIVIDUAL CHARACTER
-      var chars = i.text().split('');
-      //GET A RANDOM CHARACTER FROM THE TEXT
-      var rand = Math.floor(Math.random() * (chars.length));
-      //GET A RANDOM REPLACEMENT CHARACTER
-      var randRep = Math.floor(Math.random() * (charSet.length));
-      //CHECK TO MAKE SURE THAT THE NEW CHARACTER IS DIFFERENT FROM THE OLD
-      if(chars[rand] != charSet[randRep] && chars[rand] != ' '){
-        chars[rand] = charSet[randRep];
-      } else {
-        distortText(i);
-      }
-      //UPDATE TEXT
-      i.text(chars.join(''));
-      runs += 1;
-    }
-  });
-
 
   const services = document.querySelectorAll('[data-id]')
   services.forEach(service => {
@@ -69,8 +27,13 @@ $(function(){
   var swiper = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     preloadImages: false,
+    freeMode: true,
     lazy: true,
-    // freeMode: true,
-    watchSlidesVisibility: true
+    loop: true,
+    watchSlidesVisibility: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 });
 
